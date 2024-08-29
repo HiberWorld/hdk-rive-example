fddc89f0b0b6b353ddac296f7898ef44df44e33b
Filip Engberg <filip.engberg@hiberworld.com>
2024-08-29 13:24:44 +0200

    .

--- index.tsx
+++ index.tsx
@@ -5,6 +5,10 @@ const World = () => (
   <HNode>
     <Prefab id="plane_01" y={-2} />
     <RiveUI filename="healthbar.riv" />
+    <RiveUI filename="collect_gem.riv" />
+    <RiveUI filename="crosshair.riv" />
+    <RiveUI filename="low_health.riv" />
+    <RiveUI filename="game_over.riv" />
   </HNode>
 );
 render(<World />, {
