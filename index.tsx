722619def29ba68587e75ce3c20ae735ac117806
Filip Engberg <filip.engberg@hiberworld.com>
2024-08-29 13:52:55 +0200

    .

--- index.tsx
+++ index.tsx
@@ -1,14 +1,45 @@
 import { render } from "@hiber3d/hdk-react";
+import {
+  Distribute,
+  Ground,
+  Line,
+  Spawnpoint,
+} from "@hiber3d/hdk-react-components";
 import { HNode, Prefab, RiveUI } from "@hiber3d/hdk-react/dist";
 
 const World = () => (
   <HNode>
-    <Prefab id="plane_01" y={-2} />
-    <RiveUI filename="healthbar.riv" />
-    <RiveUI filename="collect_gem.riv" />
-    <RiveUI filename="crosshair.riv" />
-    <RiveUI filename="low_health.riv" />
-    <RiveUI filename="game_over.riv" />
+    <placedPrefab id="plane_01" z={10} y={-2} />
+    <Spawnpoint z={-20} />
+    <Line
+      z={-10}
+      delta={[0, 0, 10]}
+      numberOfItems={5}
+      renderItem={
+        <>
+          <placedPrefab id="gpl_spiked_barrel_01" scale={0.5} x={-5} y={-1} />
+          <placedPrefab id="collectible_gem_01" />
+        </>
+      }
+      renderAfter={<placedPrefab id="goal_01" />}
+    />
+    <RiveUI
+      filename="healthbar.riv"
+      horizontalAlignment="LEFT"
+      verticalAlignment="TOP"
+      maxWidth={300}
+      margin={[20, 20, 20, 20]}
+      zIndex={10}
+    />
+    <RiveUI
+      filename="collect_gem.riv"
+      horizontalAlignment="CENTER"
+      verticalAlignment="TOP"
+      maxWidth={300}
+      zIndex={20}
+    />
+    <RiveUI filename="low_health.riv" stretch="FILL" />
+    <RiveUI filename="game_over.riv" maxWidth={500} />
   </HNode>
 );
 render(<World />, {
